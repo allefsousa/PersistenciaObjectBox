8f657c161dba04073b9cf9d992bb17235d7af330
Allef sousa <allefsousa_1@hotmail.com>
2018-07-03 01:11:33 -0300

    Object box Entity e Id

--- app/src/main/java/br/com/developers/allefsousa/objectbox/Model/Estabelecimento.java
+++ app/src/main/java/br/com/developers/allefsousa/objectbox/Model/Estabelecimento.java
@@ -1,8 +1,16 @@
 package br.com.developers.allefsousa.objectbox.Model;
 
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+
 /**
  * Created by allef on 03/07/2018.
  */
-
+@Entity // anotação para dizer que a classe é uma entidade no banco de dados
 public class Estabelecimento {
+
+    @Id // anotation que define qual atributo sera o id da tabela
+    private Long id;
+
+
 }
