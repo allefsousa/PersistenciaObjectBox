51590469733d73a21096f44e69224d6ff8ad1bae
Allef sousa <allefsousa_1@hotmail.com>
2018-07-10 01:22:02 -0300

    Adicionar estabelecimento

--- app/src/main/java/br/com/developers/allefsousa/objectbox/Model/Estabelecimento.java
+++ app/src/main/java/br/com/developers/allefsousa/objectbox/Model/Estabelecimento.java
@@ -3,30 +3,24 @@ package br.com.developers.allefsousa.objectbox.Model;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
 
-
 @Entity // anotação para dizer que a classe é uma entidade no banco de dados
 public class Estabelecimento {
 
-    @Id // anotation que define qual atributo sera o id da tabela
+    @Id(assignable = true) // anotation que define qual atributo sera o id da tabela e auto incremento
     private Long id;
     private String razaoSocial;
     private String nomeFantasia;
-    private String cidade;
-    private String estado;
-    private String segmento;
 
-    public Estabelecimento() {
+    public Estabelecimento(String razaoSocial, String nomeFantasia) {
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
     }
 
-    public Estabelecimento(Long id, String razaoSocial, String nomeFantasia, String cidade, String estado, String segmento) {
+    public Estabelecimento(Long id, String razaoSocial, String nomeFantasia) {
         this.id = id;
         this.razaoSocial = razaoSocial;
         this.nomeFantasia = nomeFantasia;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.segmento = segmento;
     }
-
     public Long getId() {
         return id;
     }
@@ -49,29 +43,5 @@ public class Estabelecimento {
 
     public void setNomeFantasia(String nomeFantasia) {
         this.nomeFantasia = nomeFantasia;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getSegmento() {
-        return segmento;
-    }
-
-    public void setSegmento(String segmento) {
-        this.segmento = segmento;
     }
 }
--- app/src/main/java/br/com/developers/allefsousa/objectbox/PrincipalActivity.java
+++ app/src/main/java/br/com/developers/allefsousa/objectbox/PrincipalActivity.java
@@ -2,26 +2,41 @@ package br.com.developers.allefsousa.objectbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 import br.com.developers.allefsousa.objectbox.Model.Estabelecimento;
+
 import io.objectbox.Box;
 import io.objectbox.BoxStore;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private Box<Estabelecimento> estabelecimentoBox;
+    private Estabelecimento estabelecimento;
+    private String TAG = "ObjectBox";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
+        //Objeto Estabelecimento
+        estabelecimento = new Estabelecimento("Via Varejo","Casas Bahia");
         // recuperando a instacia do banco de dados
         BoxStore boxStore = ((myApplication) getApplication()).getBoxStore();
 
         // fazendo referencia ao Objeto/tabela do banco de dados
         estabelecimentoBox = boxStore.boxFor(Estabelecimento.class);
-
-
+        AdicionarEstabelecimento(estabelecimento); // adicionando estabelecimento
     }
+
+    //metodo para salvar estabelecimento
+    private void AdicionarEstabelecimento(Estabelecimento estabelecimento) {
+            Long id = estabelecimentoBox.put(estabelecimento);
+            Log.d(TAG, "AdicionarEstabelecimento: " + id);
+    }
+
+
+
 }
